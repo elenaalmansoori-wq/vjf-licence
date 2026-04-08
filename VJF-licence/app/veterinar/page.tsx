0d5cbae6415779323a4c44245b136c4be023710e
elenaalmansoori-wq <elena.almansoori@gmail.com>
2026-04-08 21:28:51 +0400

    Update page.tsx

--- VJF-licence/app/veterinar/page.tsx
+++ VJF-licence/app/veterinar/page.tsx
@@ -3,5 +3,5 @@ import { QUESTIONS, TEST_META } from "@/lib/questions";
 
 export default function Page() {
   const m = TEST_META.veterinar;
-  return <Quiz test="veterinar" title={m.title} photo={m.photo} alt={m.alt} questions={QUESTIONS.veterinar} />;
+  return <Quiz test="veterinar" title={m.title} photo={m.photo} alt={m.alt} photoPosition={m.photoPosition} questions={QUESTIONS.veterinar} />;
 }
